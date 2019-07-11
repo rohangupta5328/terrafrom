43bddba16c98f107306f3ff3a8e68e6ce8ffc114
Rohan Gupta <52221549+rohangupta5328@users.noreply.github.com>
2019-07-11 14:18:26 -0700

    Update variables.tf

--- exercise_1/variables.tf
+++ exercise_1/variables.tf
@@ -16,7 +16,7 @@ variable "s3_bucket_region" {
 variable "s3_bucket_name" {
   type        = "string"
   description = "Name of the S3 bucket to be created used to store the remote state file"
-  default     = "Rohan Terraform"
+  default     = "rohan-terraform"
   }
 
 variable "tag_env" {
